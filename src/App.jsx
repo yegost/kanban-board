7f920bdd538f1131fa221d52e6be40aff58ee91b
yegost <caza.alexandru@yahoo.com>
2026-03-28 23:11:42 +0000

    add board layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import { useState } from 'react'
+import Column from "./components/Column"
 
 function App() {
+  const COLUMNS = ["To Do", "In Progress", "Done"]
+
   return (
-    <div>
+    <div className='board'>
+      {COLUMNS.map((col) => {
+        <Column key={col} title={col} />
+      })}
       <h1>Kanban Board</h1>
     </div>
   )
---
+++ src/components/Column.jsx
@@ -0,0 +1,9 @@
+function Column({ title }) {
+    return(
+        <div className="column">
+            <h2>{title}</h2>
+        </div>
+    )
+}
+
+export default Column
