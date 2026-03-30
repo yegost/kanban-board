8f954dbc7ee62b5558e0045c6296b0775d3cd75a
yegost <caza.alexandru@yahoo.com>
2026-03-30 23:35:48 +0100

    fefactor css files

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './styles/global.css'
+import './styles/layout.css'
+import './styles/card.css'
+import './styles/buttons.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
